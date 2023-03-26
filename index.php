9a930c6ff514384a773ef9770e372ad8007b38f0
Jamilsoft_Dynamics <myakububauchi@gmail.com>
2023-03-26 20:58:22 +0100

    init

---
+++ index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="lib/w3/w3.css">
+    <link rel="stylesheet" href="lib/font/css/all.css">
+    <link rel="stylesheet" href="lib/bs5/css/bootstrap.min.css">
+    <style>
+        *{
+            box-sizing: border-box;
+            margin: 0;
+            font-family: serif;
+        }
+        .w3-threequarter{
+            text-align: justify;
+        }
+        @media screen and (max-width:600px) {
+            .w3-threequarter{
+                text-align: center;
+            }
+        }
+    </style>
+</head>
+<body>
+    <header class="w3-container w3-header w3-bar">
+        <span class="w3-text-orange w3-bar-item w3-xlarge w3-mobile">Code<span style="font-weight: bolder;" >ignator</span> </span>
+        <nav class="nav w3-bar-item w3-right w3-mobile">
+            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-orange w3-text-orange w3-large w3-mobile">News</a>
+            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-orange w3-text-orange w3-large w3-mobile">Discuss</a>
+            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-orange w3-text-orange w3-large w3-mobile">Contribute</a>
+            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-orange w3-text-orange w3-large w3-mobile">Learn</a>
+            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-orange w3-orange w3-round">Download</a>
+        </nav>
+    </header>
+    <div class="hero" style="background-color: whitesmoke;">
+        <br>
+        <br>
+        <br>
+        <div class="w3-container w3-mobile w3-center">
+            <h1 class="w3-text-orange w3-jumbo w3-mobile">CodeIgniter <sup>5</sup></h1>
+            <br>
+            <h3 class="w3-wide">Ever Framework with powerful features</h3>
+            <p class="w3-large">
+                Its the powerful Framework features with a small footprints, build for developers who need simple 
+                <br> 
+                and elegant toolkits to create full features of web application.
+            </p>
+            <a href="#" class="w3-button w3-border-bottom w3-hover-white w3-hover-border-orange w3-orange w3-mobile w3-round" style="width:16.66%">Learn more</a>
+            <br>
+            <br>
+            <span class="w3-bar">
+                <a href="#" class="w3-btn w3-border-bottom w3-hover-white w3-hover-border-orange"> <i class="fab fa-github">star 20113</i></a>
+                <a href="#" class="w3-btn w3-border-bottom w3-hover-white w3-hover-border-orange"> <i class="fab fa-github">fork 20113</i></a>
+            </span>
+        </div>
+        <br>
+        <br>
+    </div>
+    <br>
+    <br>
+    <div class="main w3-container w3-mobile w3-center">
+        <h1 class="w3-text-orange">Why CodeIgniter?</h1>
+        <br>
+        <br>
+        <!-- <div class="w3-content w3-mobile" style="max-width: 75%;"> -->
+
+            <div class="w3-row-padding w3-mobile">
+                <div class="w3-half">
+                    <div class="w3-row">
+                        <div class="w3-quarter">
+                            <i class="fas fa-cube w3-jumbo w3-text-orange"></i>
+                        </div>
+                        <div class="w3-threequarter">
+                            <h4 style="font-weight: bolder;">Framework with the small footprint</h4>
+                            <p class="w3-large">
+                                codeigneter 5 is a 500KB Download. plus
+                                <br>
+                                750KB for the user guide
+                            </p>
+                        </div>
+                    </div>
+                    <div class="w3-row">
+                        <div class="w3-quarter">
+                            <i class="fas fa-cubes w3-jumbo w3-text-orange"></i>
+                        </div>
+                        <div class="w3-threequarter">
+                            <h4 style="font-weight: bolder;">Simple solution over complexity</h4>
+                            <p class="w3-large">
+                                codeigneter 5 is a 500KB Download. plus
+                                <br>
+                                750KB for the user guide
+                            </p>
+                        </div>
+                    </div>
+                    <div class="w3-row">
+                        <div class="w3-quarter">
+                            <i class="fab fa-php w3-jumbo w3-text-orange"></i>
+                        </div>
+                        <div class="w3-threequarter">
+                            <h4 style="font-weight: bolder;">Exceptional Performance</h4>
+                            <p class="w3-large">
+                                codeigneter 5 is a 500KB Download. plus
+                                <br>
+                                750KB for the user guide
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="w3-half">
+                    <div class="w3-row">
+                        <div class="w3-quarter">
+                            <i class="fab fa-laravel w3-jumbo w3-text-orange"></i>
+                        </div>
+                        <div class="w3-threequarter">
+                            <h4 style="font-weight: bolder;">Framework with the small footprint</h4>
+                            <p class="w3-large">
+                                codeigneter 5 is a 500KB Download. plus
+                                <br>
+                                750KB for the user guide
+                            </p>
+                        </div>
+                    </div>
+                    <div class="w3-row">
+                        <div class="w3-quarter">
+                            <i class="fas fa-book-reader w3-jumbo w3-text-orange"></i>
+                        </div>
+                        <div class="w3-threequarter">
+                            <h4 style="font-weight: bolder;">Clear documentation</h4>
+                            <p class="w3-large">
+                                codeigneter 5 is a 500KB Download. plus
+                                <br>
+                                750KB for the user guide
+                            </p>
+                        </div>
+                    </div>
+                    <div class="w3-row">
+                        <div class="w3-quarter">
+                            <i class="fas fa-i-cursor w3-jumbo w3-text-orange"></i>
+                        </div>
+                        <div class="w3-threequarter">
+                            <h4 style="font-weight: bolder;">Framework with the small footprint</h4>
+                            <p class="w3-large">
+                                codeigneter 5 is a 500KB Download. plus
+                                <br>
+                                750KB for the user guide
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <br>
+            <br>
+            <br>
+
+            <div class="row">
+                <div class="col-md-3 p-2">
+                    <div class="w3-card p-2">
+                        <h3>Download page</h3>
+                        <a href="#" class="w3-btn w3-text-orange">Find all the Releases</a>
+                        <br>
+                        <i class="fas fa-cloud w3-xxlarge"></i>
+                    </div>
+                </div>
+                <div class="col-md-3 p-2">
+                    <div class="w3-card p-2">
+                        <h3>Clear documentation</h3>
+                        <a href="#" class="w3-btn w3-text-orange">Read the manual</a>
+                        <br>
+                        <i class="fas fa-cloud w3-xxlarge"></i>
+                    </div>
+                </div>
+                <div class="col-md-3 p-2">
+                    <div class="w3-card p-2">
+                        <h3>Get support</h3>
+                        <a href="#" class="w3-btn w3-text-orange">View the forum</a>
+                        <br>
+                        <i class="fas fa-cloud w3-xxlarge"></i>
+                    </div>
+                </div>
+                <div class="col-md-3 p-2">
+                    <div class="w3-card p-2">
+                        <h3>Add features</h3>
+                        <a href="#" class="w3-btn w3-text-orange">On github</a>
+                        <br>
+                        <i class="fas fa-cloud w3-xxlarge"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="w3-content" style="max-width: 75%;">
+            <div class="w3-row">
+                <div class="w3-half">
+                    <h3>Recent News</h3>
+                    <div class="w3-row w3-border-bottom">
+                        <div class="w3-quarter">
+                            <p class="w3-xlarge">2023.01.01</p>
+                        </div>
+                        <div class="w3-threequater">
+                            <p class="w3-text-orange w3-xlarge">codeigneter 5.0 releases</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="w3-half">
+                    <h3>Active forum Threat</h3>
+                    <div class="w3-row w3-border-bottom">
+                        <div class="w3-quarter">
+                            <p class="w3-xlarge">2023.01.01</p>
+                        </div>
+                        <div class="w3-threequater">
+                            <p class="w3-text-orange w3-xlarge">codeigneter 5.0 releases</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
