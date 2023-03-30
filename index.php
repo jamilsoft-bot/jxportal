977208c267f72795437a4f253761de556c0a0f6f
abdul <7054abdul@gmail.com>
2023-03-30 13:56:33 -0700

    update index.php

--- index.php
+++ index.php
@@ -4,35 +4,52 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="lib/w3/w3.css">
-    <link rel="stylesheet" href="lib/font/css/all.css">
-    <link rel="stylesheet" href="lib/bs5/css/bootstrap.min.css">
+    <title>Jamilx</title>
+    <link rel="stylesheet" href="assets/w3/w3.css">
+    <link rel="stylesheet" href="assets/bs5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/font/css/all.css">
+    <link rel="shortcut icon" href="jslogobird.png" type="image/x-icon">
     <style>
         *{
             box-sizing: border-box;
             margin: 0;
             font-family: serif;
         }
-        .w3-threequarter{
-            text-align: justify;
+        .w3-threequarter h4 p{
+            text-align: left;
+        }
+        .sub-main{
+            margin-left: 4vw;
         }
         @media screen and (max-width:600px) {
+            .footer
+            .footer1
+            .footer2
+            .footer3{
+                text-align: left;
+            }
+            .sub-main{
+                margin-left: none;
+            }
             .w3-threequarter{
                 text-align: center;
+            }
+            .w3-mobile{
+                display: block;
+                width: 100%;
             }
         }
     </style>
 </head>
 <body>
     <header class="w3-container w3-header w3-bar">
-        <span class="w3-text-orange w3-bar-item w3-xlarge w3-mobile">Code<span style="font-weight: bolder;" >ignator</span> </span>
+        <span class="w3-text-blue w3-bar-item w3-xlarge w3-mobile">Jamil<span style="font-weight: bolder;" >X</span> </span>
         <nav class="nav w3-bar-item w3-right w3-mobile">
-            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-orange w3-text-orange w3-large w3-mobile">News</a>
-            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-orange w3-text-orange w3-large w3-mobile">Discuss</a>
-            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-orange w3-text-orange w3-large w3-mobile">Contribute</a>
-            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-orange w3-text-orange w3-large w3-mobile">Learn</a>
-            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-orange w3-orange w3-round">Download</a>
+            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-blue w3-text-blue w3-large w3-mobile">News</a>
+            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-blue w3-text-blue w3-large w3-mobile">Discuss</a>
+            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-blue w3-text-blue w3-large w3-mobile">Contribute</a>
+            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-blue w3-text-blue w3-large w3-mobile">Learn</a>
+            <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-border-white w3-hover-border-blue w3-blue w3-round w3-mobile">Download</a>
         </nav>
     </header>
     <div class="hero" style="background-color: whitesmoke;">
@@ -40,7 +57,7 @@
         <br>
         <br>
         <div class="w3-container w3-mobile w3-center">
-            <h1 class="w3-text-orange w3-jumbo w3-mobile">CodeIgniter <sup>5</sup></h1>
+            <h1 class="w3-text-blue w3-jumbo w3-mobile">Jamilx</h1>
             <br>
             <h3 class="w3-wide">Ever Framework with powerful features</h3>
             <p class="w3-large">
@@ -48,12 +65,12 @@
                 <br> 
                 and elegant toolkits to create full features of web application.
             </p>
-            <a href="#" class="w3-button w3-border-bottom w3-hover-white w3-hover-border-orange w3-orange w3-mobile w3-round" style="width:16.66%">Learn more</a>
+            <a href="#" class="w3-button w3-border-bottom w3-hover-white w3-hover-border-blue w3-blue w3-mobile w3-round" style="width:16.66%">Learn more</a>
             <br>
             <br>
             <span class="w3-bar">
-                <a href="#" class="w3-btn w3-border-bottom w3-hover-white w3-hover-border-orange"> <i class="fab fa-github">star 20113</i></a>
-                <a href="#" class="w3-btn w3-border-bottom w3-hover-white w3-hover-border-orange"> <i class="fab fa-github">fork 20113</i></a>
+                <a href="#" class="w3-btn w3-border-bottom w3-hover-white w3-hover-border-blue"> <i class="fab fa-github">star 20113</i></a>
+                <a href="#" class="w3-btn w3-border-bottom w3-hover-white w3-hover-border-blue"> <i class="fab fa-github">fork 20113</i></a>
             </span>
         </div>
         <br>
@@ -62,21 +79,21 @@
     <br>
     <br>
     <div class="main w3-container w3-mobile w3-center">
-        <h1 class="w3-text-orange">Why CodeIgniter?</h1>
+        <h1 class="w3-text-blue">Why Jamilx?</h1>
         <br>
         <br>
         <!-- <div class="w3-content w3-mobile" style="max-width: 75%;"> -->
 
-            <div class="w3-row-padding w3-mobile">
+            <div class="w3-row">
                 <div class="w3-half">
                     <div class="w3-row">
                         <div class="w3-quarter">
-                            <i class="fas fa-cube w3-jumbo w3-text-orange"></i>
+                            <i class="fas fa-cube w3-jumbo w3-text-blue"></i>
                         </div>
-                        <div class="w3-threequarter">
+                        <div class="w3-threequarter w3-mobile">
                             <h4 style="font-weight: bolder;">Framework with the small footprint</h4>
                             <p class="w3-large">
-                                codeigneter 5 is a 500KB Download. plus
+                                codeigneter 5 is a 500KB Download. plus 
                                 <br>
                                 750KB for the user guide
                             </p>
@@ -84,27 +101,24 @@
                     </div>
                     <div class="w3-row">
                         <div class="w3-quarter">
-                            <i class="fas fa-cubes w3-jumbo w3-text-orange"></i>
+                            <i class="fas fa-cubes w3-jumbo w3-text-blue"></i>
                         </div>
                         <div class="w3-threequarter">
-                            <h4 style="font-weight: bolder;">Simple solution over complexity</h4>
+                            <h4 style="font-weight: bolder;">Architecture</h4>
                             <p class="w3-large">
-                                codeigneter 5 is a 500KB Download. plus
-                                <br>
-                                750KB for the user guide
+                                Jamilx follows the Model-Container-Service (MCS) pattern, which is a variation of the Model-View-Controller
+                                (MVC) pattern.This pattern allows for a separation of concerns and promotes modularity and flexibility.
                             </p>
                         </div>
                     </div>
                     <div class="w3-row">
                         <div class="w3-quarter">
-                            <i class="fab fa-php w3-jumbo w3-text-orange"></i>
+                            <i class="fab fa-php w3-jumbo w3-text-blue"></i>
                         </div>
                         <div class="w3-threequarter">
                             <h4 style="font-weight: bolder;">Exceptional Performance</h4>
                             <p class="w3-large">
-                                codeigneter 5 is a 500KB Download. plus
-                                <br>
-                                750KB for the user guide
+                                Developers can create reusable Container templates and use them across multiple Services.
                             </p>
                         </div>
                     </div>
@@ -113,40 +127,39 @@
                 <div class="w3-half">
                     <div class="w3-row">
                         <div class="w3-quarter">
-                            <i class="fab fa-laravel w3-jumbo w3-text-orange"></i>
+                            <i class="fab fa-laravel w3-jumbo w3-text-blue"></i>
                         </div>
                         <div class="w3-threequarter">
-                            <h4 style="font-weight: bolder;">Framework with the small footprint</h4>
+                            <h4 style="font-weight: bolder;">Database support</h4>
                             <p class="w3-large">
-                                codeigneter 5 is a 500KB Download. plus
-                                <br>
-                                750KB for the user guide
+                                Jamilx supports MySQL and SQLite databases. It uses an ORM called JamilxDB, which provides an
+                                easy-to-use interface for database operations.
                             </p>
                         </div>
                     </div>
                     <div class="w3-row">
                         <div class="w3-quarter">
-                            <i class="fas fa-book-reader w3-jumbo w3-text-orange"></i>
+                            <i class="fas fa-book-reader w3-jumbo w3-text-blue"></i>
                         </div>
                         <div class="w3-threequarter">
-                            <h4 style="font-weight: bolder;">Clear documentation</h4>
+                            <h4 style="font-weight: bolder;">Admin interface</h4>
                             <p class="w3-large">
-                                codeigneter 5 is a 500KB Download. plus
+                                Jamilx provides an admin interface for managing Modules, Users, Roles, and Application settings.
+                                The admin  
                                 <br>
-                                750KB for the user guide
+                                interface is customizable and can be extended to include custom features.
                             </p>
                         </div>
                     </div>
                     <div class="w3-row">
                         <div class="w3-quarter">
-                            <i class="fas fa-i-cursor w3-jumbo w3-text-orange"></i>
+                            <i class="fas fa-i-cursor w3-jumbo w3-text-blue"></i>
                         </div>
                         <div class="w3-threequarter">
-                            <h4 style="font-weight: bolder;">Framework with the small footprint</h4>
+                            <h4 style="font-weight: bolder;">Caching</h4>
                             <p class="w3-large">
-                                codeigneter 5 is a 500KB Download. plus
-                                <br>
-                                750KB for the user guide
+                                Jamilx provides a caching system that supports multiple backends, including file-based, APCu, and Redis.
+                                Caching can be configured on a per-Service basis.
                             </p>
                         </div>
                     </div>
@@ -162,7 +175,7 @@
                 <div class="col-md-3 p-2">
                     <div class="w3-card p-2">
                         <h3>Download page</h3>
-                        <a href="#" class="w3-btn w3-text-orange">Find all the Releases</a>
+                        <a href="#" class="w3-btn w3-text-blue">Find all the Releases</a>
                         <br>
                         <i class="fas fa-cloud w3-xxlarge"></i>
                     </div>
@@ -170,7 +183,7 @@
                 <div class="col-md-3 p-2">
                     <div class="w3-card p-2">
                         <h3>Clear documentation</h3>
-                        <a href="#" class="w3-btn w3-text-orange">Read the manual</a>
+                        <a href="#" class="w3-btn w3-text-blue">Read the manual</a>
                         <br>
                         <i class="fas fa-cloud w3-xxlarge"></i>
                     </div>
@@ -178,7 +191,7 @@
                 <div class="col-md-3 p-2">
                     <div class="w3-card p-2">
                         <h3>Get support</h3>
-                        <a href="#" class="w3-btn w3-text-orange">View the forum</a>
+                        <a href="#" class="w3-btn w3-text-blue">View the forum</a>
                         <br>
                         <i class="fas fa-cloud w3-xxlarge"></i>
                     </div>
@@ -186,7 +199,7 @@
                 <div class="col-md-3 p-2">
                     <div class="w3-card p-2">
                         <h3>Add features</h3>
-                        <a href="#" class="w3-btn w3-text-orange">On github</a>
+                        <a href="#" class="w3-btn w3-text-blue">On github</a>
                         <br>
                         <i class="fas fa-cloud w3-xxlarge"></i>
                     </div>
@@ -195,32 +208,59 @@
         </div>
         <br>
         <br>
-        <div class="w3-content" style="max-width: 75%;">
-            <div class="w3-row">
-                <div class="w3-half">
+        <div class="sub-main w3-center">
+            <h2 class="w3-text-blue w3-center">Jamil X Forum</h2>
+            <br>
+            <br>
+            <div class="w3-row w3-mobile">
+                <div class="w3-half w3-mobile">
                     <h3>Recent News</h3>
-                    <div class="w3-row w3-border-bottom">
-                        <div class="w3-quarter">
-                            <p class="w3-xlarge">2023.01.01</p>
-                        </div>
-                        <div class="w3-threequater">
-                            <p class="w3-text-orange w3-xlarge">codeigneter 5.0 releases</p>
-                        </div>
-                    </div>
+                    <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-xlarge w3-mobile">2023.01.01 &nbsp;&nbsp;&nbsp;&nbsp; codeigniter 5.0 released</a>
+                    <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-xlarge w3-mobile">2023.01.01 &nbsp;&nbsp;&nbsp;&nbsp; codeigniter 4.0 released</a>
+                    <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-xlarge w3-mobile">2023.01.01 &nbsp;&nbsp;&nbsp;&nbsp; codeigniter 5.0 released</a>
+                    <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-xlarge w3-mobile">2023.01.01 &nbsp;&nbsp;&nbsp;&nbsp; codeigniter 5.0 released</a>
+                    <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-xlarge w3-mobile">2023.01.01 &nbsp;&nbsp;&nbsp;&nbsp; codeigneter 5.0 released</a>
                 </div>
-                <div class="w3-half">
+                <div class="w3-half w3-mobile">
                     <h3>Active forum Threat</h3>
-                    <div class="w3-row w3-border-bottom">
-                        <div class="w3-quarter">
-                            <p class="w3-xlarge">2023.01.01</p>
-                        </div>
-                        <div class="w3-threequater">
-                            <p class="w3-text-orange w3-xlarge">codeigneter 5.0 releases</p>
-                        </div>
-                    </div>
+                    <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-xlarge w3-mobile">2023.01.01 &nbsp;&nbsp;&nbsp;&nbsp; codeigniter 5.0 released</a>
+                    <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-xlarge w3-mobile">2023.01.01 &nbsp;&nbsp;&nbsp;&nbsp; codeigniter 4.0 released</a>
+                    <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-xlarge w3-mobile">2023.01.01 &nbsp;&nbsp;&nbsp;&nbsp; codeigniter 5.0 released</a>
+                    <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-xlarge w3-mobile">2023.01.01 &nbsp;&nbsp;&nbsp;&nbsp; codeigniter 5.0 released</a>
+                    <a href="#" class="w3-button w3-hover-white w3-border-bottom w3-xlarge w3-mobile">2023.01.01 &nbsp;&nbsp;&nbsp;&nbsp; codeigneter 5.0 released</a>
                 </div>
             </div>
         </div>
     </div>
+
+    <br>
+    <br>
+
+    <footer class="w3-blue footer">
+        <div class="w3-bar w3-card-2 w3-container w3-text-white">
+            <div class="w3-bar-item footer1">
+                <a href="#" class="w3-button w3-bar-item w3-hover-blue w3-large w3-mobile">Policies</a>
+                <a href="#" class="w3-button w3-bar-item w3-hover-blue w3-large w3-mobile">The fine prind</a>
+                <a href="#" class="w3-button w3-bar-item w3-hover-blue w3-large w3-mobile">Security Discolures</a>
+                <a href="#" class="w3-button w3-bar-item w3-hover-blue w3-large w3-mobile">News</a>
+                <a href="#" class="w3-button w3-bar-item w3-hover-blue w3-large w3-mobile">Discuss</a>
+                <a href="#" class="w3-button w3-bar-item w3-hover-blue w3-large w3-mobile">Contributes</a>
+            </div>
+            <div class="w3-bar-item w3-right footer2">
+                <a href="#" class="w3-button w3-bar-item w3-hover-blue w3-large w3-mobile"><i class="fab fa-github"></i></a>
+                <a href="#" class="w3-button w3-bar-item w3-hover-blue w3-large w3-mobile"><i class="fab fa-github"></i></a>
+                <a href="#" class="w3-button w3-bar-item w3-hover-blue w3-large w3-mobile"><i class="far fa-messenger"></i></a>
+                <a href="#" class="w3-button w3-bar-item w3-hover-blue w3-large w3-mobile"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+        <div class="w3-center w3-margin w3-padding footer3">
+                <p class="w3-xlarge w3-text-white">&copy; 2023 Jamilx foundation. Jamilx is free open source project release under i</p>
+                <span class="w3-center w3-mobile w3-xxlarge w3-text-white"><i class="fab fa-laravel"></i></span>
+        </div>
+        
+    </footer>
+
+    <!-- <p>abdullahi &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; musa</p> -->
+
 </body>
 </html>
